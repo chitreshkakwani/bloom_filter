c2a196f6bada978f75f500249c9f35525c4b2b8e
Chitresh Kakwani <chitreshkakwani@gmail.com>
2012-02-29 14:06:01 +0530

    Added insertion and querying capability

--- src/bloom_filter.hpp
+++ src/bloom_filter.hpp
@@ -8,6 +8,18 @@
 #include <stdint.h>
 
 
+static const unsigned int bits_per_char = 8;
+static const unsigned char bit_mask[bits_per_char] = {
+							0x01,  //00000001
+							0x02,  //00000010
+							0x04,  //00000100 
+							0x08,  //00001000
+							0x10,  //00010000
+							0x20,  //00100000
+							0x40,  //01000000
+							0x80   //10000000
+					 	     };
+
 inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
 	return (x << r) | (x >> (32 - r));
@@ -25,18 +37,69 @@ class BloomFilter
 
 public:
 
-	inline void insert(const unsigned char* element, int length)
+	BloomFilter():m_bitset_size(0), m_hash_count(5)
 	{
-
 	}
 
-	template<typename T>
-		inline void insert(const T& obj)
+
+	void init(long filterSize)
+	{
+		// TODO: Calculate the optimal number of hashes
+		m_hashes = new uint32_t[m_hash_count];
+		m_bitset_size = filterSize;
+		m_bitset = new unsigned char[m_bitset_size];
+	}	
+	
+	inline void insert(const unsigned char* element, int length)
+	{
+		// Calculate indices
+		hash(element, length, m_hashes);
+		
+		int charIndex = 0;
+		int bitIndex = 0;
+		// Set the correspoding bits in bitset
+		for(int i = 0; i < m_hash_count; i++)
 		{
-			insert(reinterpret_cast<const unsigned char*>(&obj), sizeof(obj));
+			charIndex = m_hashes[i] % m_bitset_size;
+			bitIndex = charIndex % bits_per_char;
+			charIndex /= bits_per_char;
+			m_bitset[charIndex] |= bit_mask[bitIndex];
 		}
+			
+	}
 
 
+	inline bool contains(const unsigned char* element, int length)
+	{
+		hash(element, length, m_hashes);
+		int charIndex = 0;
+		int bitIndex = 0;
+		
+		for(int i=0; i < m_hash_count; i++)
+		{
+			charIndex = m_hashes[i] % m_bitset_size;
+			bitIndex = charIndex % bits_per_char;
+			charIndex /= bits_per_char;
+			unsigned char temp = m_bitset[charIndex];
+			if(!(temp & bit_mask[bitIndex]))
+				return false;
+		}
+		return true;
+	}
+
+	inline void hash(const unsigned char* data, int len, uint32_t* hashes)
+	{
+		uint32_t hash1;
+		uint32_t hash2;
+
+		murmur_hash(data, len, 0, &hash1);
+		murmur_hash(data, len, hash1, &hash2);
+	
+		for(int i = 0; i < m_hash_count; i++)
+		{
+			hashes[i] = (hash1 + i * hash2);
+		}
+	}
 
 	// Finalization mix - force all bits of a hash block to avalanche
 
@@ -109,6 +172,8 @@ private:
 
 	unsigned char* m_bitset;
 	long m_bitset_size;
+	int m_hash_count;
+	uint32_t* m_hashes;
 	long m_inserted_elements;
 };
 
